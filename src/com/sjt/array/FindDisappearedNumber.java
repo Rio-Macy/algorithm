9025255ab3dabdee5ffb305d4cfce403fb4aff85
sujuntao <2717860092@qq.com>
2022-02-13 14:49:27 +0800

    FindDisappearedNumber添加了注释

--- src/com/sjt/array/FindDisappearedNumber.java
+++ src/com/sjt/array/FindDisappearedNumber.java
@@ -4,6 +4,24 @@ import java.util.*;
 
 /**
  * @author sujuntao
+ * ##### 2、数组中消失的数字
+ *
+ * https://leetcode-cn.com/problems/find-all-numbers-disappeared-in-an-array/
+ *
+ * 找到所有数组中消失的数字
+ * 给定一个范围在  1 ≤ a[i] ≤ n ( n = 数组大小 ) 的 整型数组，数组中的元素一些出现了两次，另一些只出现一次。
+ *
+ * 找到所有在 [1, n] 范围之间没有出现在数组中的数字。
+ *
+ * 您能在不使用额外空间且时间复杂度为O(n)的情况下完成这个任务吗? 你可以假定返回的数组不算在额外空间内。
+ *
+ * 示例:
+ *
+ * 输入:
+ * [4,3,2,7,8,2,3,1]
+ *
+ * 输出:
+ * [5,6]
  */
 public class FindDisappearedNumber {
 
@@ -19,7 +37,6 @@ public class FindDisappearedNumber {
 
     /**
      * 使用Set 和 List元素来处理重复元素
-     *
      * @param nums
      * @return
      */
@@ -42,12 +59,11 @@ public class FindDisappearedNumber {
                 integerList.add(i);
             }
         }
-        System.out.println(integerSet.toString());
+
         return integerList;
     }
 
     /**
-     *
      * 不使用额外的空间
      * 利用数组的特性 用索引来记录已经出现的数字
      * @param nums
